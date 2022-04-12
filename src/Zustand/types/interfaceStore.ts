fc4b2b5c2f1d0deb892c8a251d679ab93f45eb51
Jurgen Hasmeta <58026572+Avenger22@users.noreply.github.com>
2022-04-12 16:35:56 +0200

    added structure to folders etc

---
+++ src/Zustand/types/interfaceStore.ts
@@ -0,0 +1,40 @@
+import React from "react"
+
+export default interface AppStoreState {
+
+    users: any,
+    movies: any,
+    searchTerm: string,
+    user: any,
+    userItem: any,
+    genres: any,
+    movieItem: any,
+    comments: any,
+    favorites: any,
+
+    setComments: (array:any) => void,
+    setSearchTerm: (string: string) => void,
+    setMovieItem: (data: any) => void,
+    setGenres: (genresFromServer: any) => void,
+    setMovies: (moviesFromServer: any) => void,
+    setUser: (data: any) => void,
+    setUserItem: (data:any) => void,
+    setFavorites: (favoritesFromServer: any) => void,
+
+    emailLogin: string,
+    passwordLogin: string,
+
+    handleEmailChangeLogin: (e: any) => void,
+    handlePasswordChangeLogin: (e: any) => void,
+    handleFormSubmitLogin: (e: any) => void,
+
+    userNameRegister: string,
+    emailRegister: string,
+    passwordRegister: string,
+
+    handleFormSubmitRegister: (e: any) => void,
+    handleUserNameRegister: (e: any) => void,
+    handleEmailRegister: (e: any) => void,
+    handlePasswordChangeRegister: (e: any) => void,
+
+}
