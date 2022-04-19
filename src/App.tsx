06ad2cc3bf2434048e1c1311443b507592f588d6
Visard123 <92378096+Visard123@users.noreply.github.com>
2022-04-19 12:15:12 +0200

    stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 // #region "Importing"
-import HomePage from './Pages/Home/HomePage'
-import MoviePage from './Pages/Movie/MoviePage'
-import LoginPage from './Pages/Login/LoginPage'
-import RegisterPage from './Pages/Register/RegisterPage'
-import GenrePage from './Pages/Genre/GenrePage'
-import ProfilePage from './Pages/Profile/ProfilePage'
-import ErrorPage from './Pages/Error/ErrorPage'
+import HomePage from "./Pages/Home/HomePage";
+import MoviePage from "./Pages/Movie/MoviePage";
+import LoginPage from "./Pages/Login/LoginPage";
+import RegisterPage from "./Pages/Register/RegisterPage";
+import GenrePage from "./Pages/Genre/GenrePage";
+import ProfilePage from "./Pages/Profile/ProfilePage";
+import ErrorPage from "./Pages/Error/ErrorPage";
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom"
+} from "react-router-dom";
 
-import './App.css'
-import { useStore } from './Zustand/store'
+import "./App.css";
+import { useStore } from "./Zustand/store";
 // #endregion
 
 function App() {
-
-  const { setUser } = useStore()
+  const { setUser } = useStore();
 
   function validateUser() {
-
     if (localStorage.token) {
-
       fetch("http://localhost:4000/validate", {
         headers: {
           Authorization: localStorage.token,
@@ -33,90 +30,98 @@ function App() {
       })
         .then((resp) => resp.json())
         .then((data) => {
-
           if (data.error) {
             console.log("Validation failed.");
-          } 
-          
-          
-          else {
+          } else {
             setUser(data);
           }
-
-      });
-
+        });
     }
-
   }
 
   return (
-
     <>
+      <MoviePage />
+      <Routes>
+        <Route index element={<Navigate replace to="/home" />} />
 
-        <Routes>
+        <Route
+          path="/home"
+          element={
+            //@ts-ignore
+            <HomePage validateUser={validateUser} />
+          }
+        />
 
-            <Route index element={<Navigate replace to="/home" />} />
-
-            <Route path="/home" element={
-                //@ts-ignore
-                <HomePage validateUser = {validateUser} />} 
-            />
-
-            {/* <Route path="/videos/:id" element={
+        {/* <Route path="/videos/:id" element={
                 //@ts-ignore
                 <MoviePage validateUser = {validateUser} />} 
             /> */}
 
-            <Route path="/video" element={
-                //@ts-ignore
-                <MoviePage validateUser = {validateUser} />} 
-            />
+        <Route
+          path="/video"
+          element={
+            //@ts-ignore
+            <MoviePage validateUser={validateUser} />
+          }
+        />
 
-            {/* <Route path="/users/:id" element={
+        {/* <Route path="/users/:id" element={
                 //@ts-ignore
                 <ProfilePage validateUser = {validateUser} />} 
             /> */}
 
-            <Route path="/profile" element={
-                //@ts-ignore
-                <ProfilePage validateUser = {validateUser} />} 
-            />
+        <Route
+          path="/profile"
+          element={
+            //@ts-ignore
+            <ProfilePage validateUser={validateUser} />
+          }
+        />
 
-            <Route path="/login" element={
-                <LoginPage 
-                    //@ts-ignore
-                    validateUser = {validateUser}
-                />} 
+        <Route
+          path="/login"
+          element={
+            <LoginPage
+              //@ts-ignore
+              validateUser={validateUser}
             />
+          }
+        />
 
-            <Route path="/register" element={
-                <RegisterPage 
-                //@ts-ignore
-                    validateUser = {validateUser}
-                />} 
+        <Route
+          path="/register"
+          element={
+            <RegisterPage
+              //@ts-ignore
+              validateUser={validateUser}
             />
+          }
+        />
 
-            {/* <Route path="/genres/:name" element={
+        {/* <Route path="/genres/:name" element={
                 //@ts-ignore
                 <GenrePage validateUser = {validateUser} />} 
             /> */}
 
-            <Route path="/genres" element={
-                //@ts-ignore
-                <GenrePage validateUser = {validateUser} />} 
-            />
+        <Route
+          path="/genres"
+          element={
+            //@ts-ignore
+            <GenrePage validateUser={validateUser} />
+          }
+        />
 
-            <Route path="*" element={
-                //@ts-ignore
-                <ErrorPage validateUser = {validateUser} />} 
-            />
-
-        </Routes>
-
+        <Route
+          path="*"
+          element={
+            //@ts-ignore
+            <ErrorPage validateUser={validateUser} />
+          }
+        />
+      </Routes>
     </>
-
-  )
-
+  );
 }
 
-export default App
+export default App;
